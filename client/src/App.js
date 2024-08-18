feb16872d65a312aad85ba9877c97c3d05ac4f4f
Hinoden <krwong3@gmail.com>
2024-08-18 09:38:51 -0700

    Created a Log Out button

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import {useState} from "react";
+import Button from '@mui/material/Button';
 import Welcome from "./components/Greeting.js";
 import Login from "./components/Login.js";
 import SignUp from "./components/SignUp.js";
 import './App.css';
+import axios from "./api/axios.js";
 
 function App() {
   const [myBool, setmyBool] = useState(true);
@@ -16,10 +18,24 @@ function App() {
   function toggleBool() {
     setmyBool(!myBool);
   }
+
+  const logOut = () => {
+    axios.post("http://localhost:3500/logout")
+    .then(response => {
+      setAuth(false);
+      localStorage.removeItem("token");
+      setCurrentForm('login');
+      console.log(response.data.message);
+    })
+    .catch(error => {
+      console.error("There was an error logging out!", error);
+    })
+  }
+
   return (
     <div className="App">
       {auth ? (
-        <h>Please</h>
+        <Button variant="contained" onClick={logOut}>Log Out</Button>
       ) : (
         <>
       {myBool ? (
