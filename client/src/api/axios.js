f9247433e92185cce4d60a87afa8fe24cbaf2d69
Hinoden <151482278+Hinoden@users.noreply.github.com>
2024-11-26 22:27:04 -0800

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://book-collection-web-project-api.vercel.app/'
+    baseURL: 'https://book-collection-web-project-api.vercel.app'
 });
